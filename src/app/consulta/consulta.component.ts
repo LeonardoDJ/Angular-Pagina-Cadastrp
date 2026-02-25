0e8f878de6a16f6a72269c5abb5897dce63e1cc9
Leonardo <leonardodjdev@gmail.com>
2026-02-25 15:09:44 -0300

    Adicionando botao deletar no formulario e fazendo  funcionar

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -51,4 +51,13 @@ export class ConsultaComponent implements OnInit{
     this.router.navigate(['/cadastro'], {queryParams: { "id": id } } )
   }
 
+  preparaDeletar(cliente: Cliente){
+    cliente.deletando = true;
+  }
+
+  deletar(cliente: Cliente){
+    this.service.deletar(cliente);
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
+
 }
